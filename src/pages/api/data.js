6181a8a0ff5d45f3d92aff0182bcc7313e179f24
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-05-01 23:31:11 -0700

    initial commit

---
+++ src/pages/api/data.js
@@ -0,0 +1,59 @@
+import {Database} from '/src/lib/sensor_data';
+
+// For SSE
+const INTERVAL = 50; //ms
+const ID = "sensor1";
+
+
+export default function handler(req, res) {
+  const db = new Database();
+
+  if (req.method == 'POST') {
+    const value = req.body.value;
+    // console.log(`received: ${value}`);
+    db.appendData(value);
+    res.headers = [
+      {
+        key: 'Keep-Alive',
+        value: false,
+      }
+    ];
+    return res.status(200).end();
+  }
+
+  if (req.method == 'GET') {
+    // For any other verb, just treat as GET
+    res.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive',
+    });
+
+    // Keep sending SSE on a timer
+    setInterval(() => {
+      sendSSE(res, ID, db.getData());
+    }, INTERVAL);
+
+    // Send initial SSE
+    return sendSSE(res, ID, db.getData());
+  }
+}
+
+
+/**
+ * Sends SSE to browser.
+ * @param res Response object
+ * @param id Optional, used to identify a specific event source
+ * @param data The payload that will be sent (available on the SSE `data` prop
+ *             as a JSON encoded string).
+ */
+function sendSSE(res, id, value) {
+  if (value > -1) {
+    console.log(`sending value: ${value}`);
+  }
+  res.write('id: ' + id + '\n');
+  res.write('data: ' + value.toString() + '\n');
+  res.write('\n');
+  res.flush();
+}
+
---
+++ src/pages/index.js
@@ -0,0 +1,84 @@
+import { Inter } from 'next/font/google';
+
+import {useState, useEffect} from 'react';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Index() {
+
+  const [val, setVal] = useState(50)
+
+  useEffect(() => {
+    const sensor = document.getElementById('sensor1');
+    let eventSrc;
+    let state;
+
+    let connect = () => {
+      console.log(`connecting...`);
+      eventSrc = new EventSource('/api/data');
+
+      eventSrc.onmessage = (event) => {
+        const value = parseInt(event.data, 10);
+        if (value > -1) {
+          console.log(`value: ${parseInt(event.data, 10)}`);
+          // setVal(value);
+          sensor.value = event.data;
+        }
+      };
+
+      eventSrc.onerror = (error) => {
+        console.error(`error: ${error}`);
+        eventSrc.close();
+        connect();
+      };
+    }
+
+    setInterval(() => {
+      if (eventSrc.readyState === state) return;
+
+      state = eventSrc.readyState;
+      console.log(`readyState: ${state}`);
+
+      if (state === 0) {
+        console.log(`readyState: connecting`);
+      } else if (state === 1) {
+        console.log(`readyState: open`);
+      } else {
+        console.log(`readyState: ${eventSrc.readyState}`);
+        connect();
+      }
+    }, 1000);
+
+    connect();
+  });
+
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
+          Sensor App Demo&nbsp;
+        </p>
+
+        <div className="text-center">
+          <div className="mt-40">
+            <label htmlFor="sensor">Live Sensor Data</label>
+            <br/>
+            <br/>
+            <br/>
+            <input type="range" min="0" max="100" step="1" value={val} readOnly id="sensor1"/>
+          </div>
+        </div>
+        <br/>
+
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black">
+          <p className="place-items-center gap-2 p-8 lg:pointer-events-auto">
+            &copy; 2023, Cormac Pujals
+          </p>
+        </div>
+      </div>
+
+    </main>
+  )
+}
